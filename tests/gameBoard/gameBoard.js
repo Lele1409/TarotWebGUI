505aa47b53ed0eb21c440903909c2def2ee55404
Lele1409 <leander.decone@gmail.com>
2023-08-13 11:56:33 +0200

    added username

--- tests/gameBoard/gameBoard.js
+++ tests/gameBoard/gameBoard.js
@@ -3,9 +3,9 @@ async function createOtherPlayersHands(playerCount) {
     const otherPlayersHands = document.querySelector('other-players-hands-wrapper')
 
     const otherPlayersHandsPositions = {
-        3: [[10, 35, 65], [10, 65, 35]],
-        4: [[30, 20, 80], [10, 40, 60], [30, 80, 20]],
-        5: [[30, 20, 80], [10, 35, 65], [10, 65, 35], [30, 80, 20]]
+        3: [[20, 25, 75], [20, 75, 25]],
+        4: [[45, 15, 85], [20, 25, 75], [45, 85, 15]],
+        5: [[45, 15, 85], [20, 25, 75], [20, 75, 25], [45, 85, 15]]
     }
 
     const otherPlayersHandsPosition = otherPlayersHandsPositions[nOtherPlayers + 1]
@@ -13,21 +13,54 @@ async function createOtherPlayersHands(playerCount) {
     for (i = 0; i < nOtherPlayers; i++) {
         let otherPlayerHand = document.createElement('other-player-hand')
 
-        otherPlayerHand.style.top = `${otherPlayersHandsPosition[i][0]}%`
-        otherPlayerHand.style.left = `${otherPlayersHandsPosition[i][1]}%`
-        otherPlayerHand.style.right = `${otherPlayersHandsPosition[i][2]}%`
+        otherPlayerHand.style.top = `${otherPlayersHandsPosition[i][0]}dvh`
+        otherPlayerHand.style.left = `${otherPlayersHandsPosition[i][1]}dvw`
+        otherPlayerHand.style.right = `${otherPlayersHandsPosition[i][2]}dvw`
 
+        let profilePicture = document.createElement('other-player-profile-picture')
+        profilePicture.style.setProperty('--profile-picture', "url('https://placehold.jp/3d4070/ffffff/150x150.png')")  // TODO: replace with getUserProfilePicture()
+
+        let userNameDisplay = document.createElement('other-player-username')
+        let username = 'example user 123 123'  // TODO: replace with getUsername()
+        userNameDisplay.setAttribute('username', username)
+        
+        let angleStep = 10
+        let angle = username.length/2 * -(angleStep) + angleStep/2
+        for (letter of username) {
+            userNameDisplay.innerHTML += `<span style="transform: rotate(${angle}deg) scaleX(-1);">${letter}</span>`
+            angle += angleStep
+        }
+
+        profilePicture.appendChild(userNameDisplay)
+        otherPlayerHand.appendChild(profilePicture)
         otherPlayersHands.appendChild(otherPlayerHand)
     }
 }
 
 async function createOwnHand() {
-    
+    const observer = new MutationObserver(onOwnHandMutation)
+
+    observer.observe( document.querySelector('own-hand'), {
+        childList: true,
+        subtree: true
+    })
+
 }
-// UFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF top and bottom
-async function addCardToOwnHand(cardSelector) {
-    let ownHand = document.querySelector(own-hand)
-    await setCardPosition(cardSelector, )
+
+async function onOwnHandMutation(mutations) {
+    const upperRow = document.querySelector('own-hand-upper-row')
+    const lowerRow = document.querySelector('own-hand-lower-row')
+
+    const upperNodes = upperRow.childNodes.length
+    const lowerNodes = lowerRow.childNodes.length
+
+    try {
+        if (upperNodes > lowerNodes || upperNodes <= 3) {
+            lowerRow.insertBefore(upperRow.lastElementChild, lowerRow.firstElementChild)
+        } else if (upperNodes < lowerNodes - 1) {
+            upperRow.appendChild(lowerRow.firstElementChild)
+        }
+    } catch (error) {}
 }
 
 async function run() {
